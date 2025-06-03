e74589269380d0606b37cfc4e0b9b4c373558557
Abigael <abigaelbusienei083@gmail.com>
2025-06-03 14:03:44 +0300

    django project

---
+++ Phone_database/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from Phone_database.models import Student
+from Phone_database.serializer import StudentSerializer
+
+
+# Create your views here.
+@api_view(['POST'])
+def save(request):
+    serializer = StudentSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def fetch(request):
+  
+    students = Student.objects.all()
+    serializer = StudentSerializer(students, many=True)
+    return Response(serializer.data)
